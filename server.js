14edd6f294be727350a23571631e1cbaaea2dfd6
Aasma <aasma.sh@gmail.com>
2018-08-26 08:01:44 +0530

    Added Update code

--- server.js
+++ server.js
@@ -110,8 +110,56 @@ app.delete('/todos/:id',function(req,res
   }
  });
 
+ //PUT  /todos/:id
+
+ app.put('/todos/:id', function (req, res)
+ {
+   var todoId = parseInt(req.params.id,10);
+   var matchedtodo = _.findWhere(todos, {id:todoId});
+   var body = _.pick (req.body,'description','completed');
+   var validAttributes = {};
+      
+   if (!matchedtodo){
+      return  res.status(404).send;
+   }
+   //validation for completed property
+
+    if (body.hasOwnProperty('completed') &&  _.isBoolean(body.completed))
+        {
+            validAttributes.completed= body.completed;
+        }
+                else if (body.hasOwnProperty('completed') )
+        {
+           res.status(400).send();
+        }
+
+      //validation for description property
+      
+      if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0)
+        {
+          validAttributes.description = body.description;
+        }
+        else if (body.hasOwnProperty('description'))
+        {
+              res.status(400).send();
+        }
+
+         //update the matched todo to valid attributes semd by user
+
+        _.extend(matchedtodo,validAttributes);
+        res.send(matchedtodo);
+
+
+    });
+
+ 
+
+
+
+
 app.listen(PORT,function(){
   console.log('Express Listening on Port ' + PORT + '!');
+
 });
 
 
